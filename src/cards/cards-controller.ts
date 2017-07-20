3a8212d836d141fbdcbe665383654d3b7d00c713
mrgigabyte <nandan3101@gmail.com>
2017-07-20 10:27:53 +0530

    added required modules

---
+++ src/cards/cards-controller.ts
@@ -0,0 +1,36 @@
+import * as Hapi from "hapi";
+import * as Boom from "boom";
+import * as Jwt from "jsonwebtoken";
+import * as GoogleAuth from "google-auth-library";
+
+import { IServerConfigurations } from "../configurations";
+
+
+export default class UserController {
+
+    private configs: IServerConfigurations;
+    private database: any;
+    private user: any;
+
+    constructor(configs: IServerConfigurations, database: any) {
+        this.database = database;
+        this.configs = configs;
+    }
+
+    public try(request: Hapi.Request, reply: Hapi.Base_Reply) {
+        // console.log(this.database);
+        const a = this.database.user.build({
+            email: "vidur@navvv",
+            name: "vidur singla",
+            profilePicture: "dasas",
+            googleUserId: 1234,
+            facilitator: 1
+        });
+        a.save().then((res) => {
+            console.log('very good');
+            return reply(res);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+}
---
+++ src/cards/schemas.ts
@@ -0,0 +1,17 @@
+import * as Joi from "joi";
+
+export const userSchema:Joi.ObjectSchema = Joi.object({
+    id: Joi.number(),
+    name: Joi.string(),
+    email: Joi.string().email(),
+    profilePicture: Joi.string().uri(),
+    googleUserId: Joi.string(),
+    facilitator: Joi.bool()
+});
+
+export const noteSchema:Joi.ObjectSchema = Joi.object({
+    id: Joi.number().required(),
+    text: Joi.string().required(),
+    createdAt: Joi.date().required(),
+    name: Joi.string().required(),
+});
